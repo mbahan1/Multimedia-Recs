822e9cec6c9e257aaa7be3dface5b3fdd72f24fb
bahanm <bahanm@gmail.com>
2022-04-02 12:35:21 -0700

    added home and views urls.

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-
+    path('', views.Home.as_view(), name="home"),
+    path('about/', views.About.as_view(), name="about"),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views import View
+from django.http import HttpResponse
 
 # Create your views here.
+class Home(View):
+    def get(self, request):
+        return HttpResponse("MediaRecs Home")
+
+class About(View):
+    def get(self, request):
+        return HttpResponse("About MediaRecs")
