a533bc35ff28ccb47a6fa88556b7097bc2f91093
bahanm <bahanm@gmail.com>
2022-04-05 19:19:38 -0700

    added url routes for review CRUD.

--- main_app/urls.py
+++ main_app/urls.py
@@ -10,4 +10,12 @@ urlpatterns = [
     path('shows/<int:pk>/update', views.Show_Update.as_view(), name="show_update"),
     path('shows/<int:pk>/delete', views.Show_Delete.as_view(), name="show_delete"),
     path('user/<username>/', views.profile, name='profile'),
+    path('reviews/', views.reviews_index, name='reviews_index'),
+    path('reviews/<int:review_id>', views.reviews_show, name='reviews_show'),
+    path('reviews/create/', views.ReviewCreate.as_view(), name='reviews_create'),
+    path('reviews/<int:pk>/update/', views.ReviewUpdate.as_view(), name='reviews_update'),
+    path('reviews/<int:pk>/delete/', views.ReviewDelete.as_view(), name='reviews_delete'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+    path('signup/', views.signup_view, name='signup'),
 ]
