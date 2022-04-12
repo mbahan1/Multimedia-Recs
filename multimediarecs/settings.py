51af792c4dd3ed587f88cec1335f727a0d8a2ddf
bahanm <bahanm@gmail.com>
2022-04-12 04:45:48 -0700

    troubleshooting crispy

--- multimediarecs/settings.py
+++ multimediarecs/settings.py
@@ -64,8 +64,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'main_app',
     "crispy_forms",
+    'main_app',
 ]
 
 CRISPY_TEMPLATE_PACK = 'uni_form'
